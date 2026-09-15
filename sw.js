20bcee6b9052c208943e83f8fc0bb3ca40b7f6f4
hhenson76 <131893022+hhenson76@users.noreply.github.com>
2026-09-15 13:37:07 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Bump CACHE when you change index.html so installed copies pick up the new build. */
-const CACHE = "recon-calc-v4-2";
+const CACHE = "recon-calc-v4-3";
 const ASSETS = [
   "./index.html",
   "./manifest.webmanifest",
